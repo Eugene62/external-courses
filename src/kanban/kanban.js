dcf1bc3309bb99a8cce34bd2c0be170fb2addf84
Eugene <mirohin.e@gmail.com>
2020-11-14 12:58:49 +0300

    доделал dropdonw меню

--- src/kanban/kanban.js
+++ src/kanban/kanban.js
@@ -1,10 +1,12 @@
 "use strict"
 
 const list = document.createElement('ul');
+list.setAttribute('class','testUl');
 const listItem1 = document.createElement('li');
 const listItem2 = document.createElement('li');
 const listItem3 = document.createElement('li');
 const listItem4 = document.createElement('li');
+const divForUl = document.createElement('div');
 
 listItem1.innerText = "My Profile";
 listItem2.innerText = "User Settings";
@@ -16,18 +18,24 @@ list.appendChild(listItem2);
 list.appendChild(listItem3);
 list.appendChild(listItem4);
 
-const menuContainer = document.getElementById('menuContainer');
-
-function createNewList(){
-  console.log("createNewList");
-};
-
-function showMenu(){
-  console.log("showMenu");
-}
+const menuContainer = document.getElementById("menuContainer");
 
 const button = document.getElementById("buttonMenu");
+const shevron = document.getElementById('shevron');
 
-const showList = () => menuContainer.appendChild(list);
+const showList = () => { 
+  menuContainer.appendChild(list);
+}
 
-button.addEventListener('click', showList);
+const hideList = () => {
+  list.remove();
+} 
+
+button.addEventListener('click', () => {
+  shevron.classList.toggle('shevron_active')
+    if(shevron.classList.contains('shevron_active') ) {
+    showList();
+  } else {
+    hideList();
+  }
+});
